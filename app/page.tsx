2c350c6eade7872bca06b76f099298153aa9a024
Maranatha Odai <moodai@st.ug.edu.gh>
2025-12-24 20:43:51 +0000

    Implemented the section for universities data

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ForUniversitiesSection from "./ForUniversitiesSection";
 
 import ProblemSection from "./ProblemSection";
 import Hero from "./Hero";
@@ -13,6 +14,7 @@ export default function Home() {
       <ProblemSection />
       <SolutionSection />
       <ForStudentsSection />
+      <ForUniversitiesSection />
     </>
   );
 }
